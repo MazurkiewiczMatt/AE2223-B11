1dab30e3cd6136acc402823539b84aac855a78c5
FrankMeijering <frank.meijering@ziggo.nl>
2021-03-10 16:19:33 +0100

    The graphs work again.

--- Scrap.py
+++ Scrap.py
@@ -34,81 +34,31 @@ for i in range(no_chirps):  # Each i is one chirp. i ranges from 0 up to and inc
         temp_lst.append(j[length_chirp*i:length_chirp*(i+1)])   # Add the data that correspond to the current chirp
     chirps.append(temp_lst)
 
-
-'''
-y1 = []
-for i in range(len(y1_1)):
-    if i <= 128:   # first chirp
-        y1.append(y1_1[i])
-x1 = np.linspace(0, 12.1, len(y1))
-
-y1_im = []
-for i in range(len(y1_2)):
-    if i <= 128:     # first chirp
-        y1_im.append(y1_2[i])
-x1_im = np.linspace(0, 12.1, len(y1_im))
-
-y2 = []
-for i in range(len(y2_1)):
-    if i <= 128:   # first chirp
-        y2.append(y2_1[i])
-x2 = np.linspace(0, 12.1, len(y2))
-
-
-
-# --------------------------------------------------------------
-dt = 12.1 / len(y1)
-n = len(x1)
-
-f_hat = np.fft.fft(y1, n)
-PSD = f_hat * np.conj(f_hat) / n
-freq = (1/(dt*n)) * np.arange(n)
-L = np.arange(1, np.floor(n/2), dtype='int')
+# --------------------------------- PLOT DATA --------------------------------
+# Another way of getting the duration would be either '12.1 / 128' or 'radar_time[1] - radar_time[0]'.
+# These are not the same but I don't know what the difference is.
+duration = int(str(radar_msg[1].ts - radar_msg[0].ts))/1e9  # seconds. Originally is type 'genpy.rostime.Duration'.
+t = np.linspace(0, duration, len(chirps[0][0]))    # x-axis [seconds]
+y = chirps[0][0]
 
 plt.suptitle('RADAR DATA')
+plt.subplot(1, 2, 1)
+plt.plot(t, y)
+plt.title('rx1_re (raw)')
+plt.xlabel('Time [s]')
+plt.ylabel('no idea')
 
-plt.subplot(2, 4, 1)
-plt.title('data_rx1_re (raw)')
-plt.plot(x1, y1, 0)
+dt = duration / len(y)
+n = len(t)
 
-plt.subplot(2, 4, 2)
-plt.title("data_rx1_re (transformed)")
-plt.plot(freq[L], PSD[L], 1)
-
-
-dt = 12.1 / len(y2)
-n = len(x2)
-
-f_hat = np.fft.fft(y2, n)
+f_hat = np.fft.fft(y, n)
 PSD = f_hat * np.conj(f_hat) / n
-freq = (1/(dt*n)) * np.arange(n)
+freq = (1/(dt*n)) * np.arange(n)   # Hz
 L = np.arange(1, np.floor(n/2), dtype='int')
 
-plt.subplot(2, 4, 5)
-plt.title('data_rx2_re (raw)')
-plt.plot(x2, y2, 0)
-
-plt.subplot(2, 4, 6)
-plt.title("data_rx2_re (transformed)")
+plt.subplot(1, 2, 2)
 plt.plot(freq[L], PSD[L], 1)
-
-
-dt = 12.1 / len(y1_im)
-n = len(x1_im)
-
-f_hat = np.fft.fft(y1_im, n)
-PSD = f_hat * np.conj(f_hat) / n
-freq = (1/(dt*n)) * np.arange(n)
-L = np.arange(1, np.floor(n/2), dtype='int')
-
-plt.subplot(2, 4, 3)
-plt.title('data_rx1_im (raw)')
-plt.plot(x1_im, y1_im, 0)
-
-plt.subplot(2, 4, 4)
-plt.title("data_rx1_im (transformed)")
-plt.plot(freq[L], PSD[L], 1)
-
-
+plt.title('rx1_re (transformed)')
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('Density [no idea]')
 plt.show()
-'''
