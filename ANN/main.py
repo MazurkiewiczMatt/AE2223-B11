de2844ca9ceb24410c42d0d5be1b57b9017249c0
FrankMeijering <frank.meijering@ziggo.nl>
2021-04-02 15:34:14 +0200

    working version!

--- ANN/main.py
+++ ANN/main.py
@@ -17,7 +17,7 @@ def get_folder_file(folder, file):
     path = get_file(extension)
     return path
 
-
+# Load the training and validation data
 def load_data(file_):
     directory_ = get_file(file_)
     data = pd.read_csv(directory_)  #r"C:\Users\frank\Documents\TU Delft\Year 2\Q3\Project\Github\ANN\x.csv"
@@ -46,7 +46,7 @@ def load_data(file_):
     dummy, m_train = X_train.shape
     return n, m, X_train, Y_train, X_valid, Y_valid
 
-
+# Initializes the weights and bias for the input values
 def init_values():
     W1 = np.random.rand(HIDDEN_LAYER, INPUT_LAYER) - 0.5
     b1 = np.random.rand(HIDDEN_LAYER, 1) - 0.5
@@ -54,39 +54,40 @@ def init_values():
     b2 = np.random.rand(OUTPUT_LAYER, 1) - 0.5
     return W1, b1, W2, b2
 
-
-def max_val_function(Z):
-    return np.maximum(Z, 0)
-
-
+# Changes Z to probabilities
 def softmax(Z):
     A = np.exp(Z) / sum(np.exp(Z))
     return A
 
-
+# 
 def cubic(Z):
     return np.power(Z, 3)
 
-
+# Make a forward prediction by adding weighted sums of the parameters
 def forward_prop(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
     A1 = max_val_function(Z1)
     Z2 = W2.dot(A1) + b2
     A2 = softmax(Z2)
     return Z1, A1, Z2, A2
-
-
+    
+# Returns 0 for everything below 0, and the value for everything above 0
+# Is an activation function
+def max_val_function(Z):
+    return np.maximum(Z, 0)
+    
+# Derivative of the max_val_function:
 def larger_val_function(Z):
     return Z > 0
 
-
+# Output is the zero vector except a 1 for the correct parameter
 def one_hot(Y):
     one_hot_Y = np.zeros((Y.size, Y.max() + 1))
     one_hot_Y[np.arange(Y.size), Y] = 1
     one_hot_Y = one_hot_Y.T
     return one_hot_Y
 
-
+# Check how good the predictions are and adjust the weights accordingly
 def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
     one_hot_Y = one_hot(Y)
     dZ2 = A2 - one_hot_Y
@@ -97,7 +98,7 @@ def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
     db1 = 1 / m * np.sum(dZ1)
     return dW1, db1, dW2, db2
 
-
+# Update the values where necessary
 def update_values(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
     W1 = W1 - alpha * dW1
     b1 = b1 - alpha * db1
@@ -105,16 +106,16 @@ def update_values(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
     b2 = b2 - alpha * db2
     return W1, b1, W2, b2
 
-
+# Eliminates negative probabilities
 def get_predictions(A2):
     return np.argmax(A2, 0)
 
-
+# Number of correctly guesses parameters, normalised to probabilities
 def accuracy(predictions, Y):
     print(predictions, Y)
     return np.sum(predictions == Y) / Y.size
 
-
+# Actual learning process / iteration of the ANN:
 def gradient_descent(X, Y, alpha, iterations):
     W1, b1, W2, b2 = init_values()
     for i in range(iterations):
@@ -122,24 +123,31 @@ def gradient_descent(X, Y, alpha, iterations):
         dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
         W1, b1, W2, b2 = update_values(
             W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
-        if i % 20 == 0:
+        if i % 20 == 0:  # prints out every 20th iteration to see progress in the learning curve
             print("Iteration: ", i)
             predictions = get_predictions(A2)
             print(accuracy(predictions, Y))
     return W1, b1, W2, b2
 
-
+# Make a forward prediction by using forward propagation
 def make_predictions(X, W1, b1, W2, b2):
     _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
     predictions = get_predictions(A2)
     return predictions
 
-
+# Function which predicts what a given image is
 def test_prediction(index, W1, b1, W2, b2, X, Y):
     prediction = make_predictions(X[:, index, None], W1, b1, W2, b2)
     label = Y[index]
     
-    if prediction == [0]:
+    names = ['A380', 'AN225', 'B747', 'B787', 'Beluga', 'C130', 'F16', 'Fokker', 'PHLAB', 'SS100']
+
+    for p in range(10):
+        if prediction == [p]:
+            prediction = names[p]
+        if label == p:
+            label = names[p]
+    '''if prediction == [0]:
         prediction = 'A380'
     elif prediction == [1]:
         prediction = 'AN225'
@@ -159,8 +167,9 @@ def test_prediction(index, W1, b1, W2, b2, X, Y):
         prediction = 'PHLAB'
     elif prediction == [9]:
         prediction = 'SS100'
+    '''
 
-    if label == 0:
+    '''if label == 0:
         label = 'A380'
     elif label == 1:
         label = 'AN225'
@@ -180,6 +189,7 @@ def test_prediction(index, W1, b1, W2, b2, X, Y):
         label = 'PHLAB'
     elif label == 9:
         label = 'SS100'
+    '''
 
     print("Prediction: ", prediction)
     print("Real: ", label)
@@ -190,18 +200,10 @@ INPUT_LAYER = 100*100  # 100x100 pixels
 HIDDEN_LAYER = 90   # Hidden layer can maximally be ~99
 OUTPUT_LAYER = 10
 
+# Load the coefficients
 n, m, X_train, Y_train, X_valid, Y_valid = load_data('x.csv')
 W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.01, 300)
 
+# To test the ANN with test data
 for i in range(100-HIDDEN_LAYER):
     test_prediction(i, W1, b1, W2, b2, X_valid, Y_valid)
-
-'''
-test_prediction(9, W1, b1, W2, b2, X_train, Y_train)
-test_prediction(5, W1, b1, W2, b2, X_train, Y_train)
-test_prediction(1, W1, b1, W2, b2, X_valid, Y_valid)
-test_prediction(2, W1, b1, W2, b2, X_valid, Y_valid)
-test_prediction(2, W1, b1, W2, b2, X_valid, Y_valid)
-test_prediction(2, W1, b1, W2, b2, X_valid, Y_valid)
-test_prediction(2, W1, b1, W2, b2, X_valid, Y_valid)
-'''
