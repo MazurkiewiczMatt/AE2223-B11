7bb5e2b6b05b76ae24b825fe9ee736f77b363c83
blueikeamug <ic1206@outlook.com>
2021-06-07 15:05:38 +0300

    minor changes in README to reflect current status + changed pose_optitrack to be single trajectory instead of nothing at all

--- pose_optitrack.py
+++ pose_optitrack.py
@@ -1,34 +1,8 @@
+import os
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-def quaternion_rotation_matrix(Q):
-    # z = a + bi + cj + dk = w + xi + zj + yk in our case bc of OptiTrack wiki
-    a = Q[0]  #
-    b = Q[1]  #
-    c = Q[3]  #
-    d = Q[2]  #
-    # First row of the rotation matrix
-    r00 = a ** 2 + b ** 2 - c ** 2 - d ** 2
-    r01 = 2 * b * c - 2 * a * d
-    r02 = 2 * b * d + 2 * a * c
-
-    # Second row of the rotation matrix
-    r10 = 2 * b * c + 2 * a * d
-    r11 = a ** 2 - b ** 2 + c ** 2 - d ** 2
-    r12 = 2 * c * d - 2 * a * b
-
-    # Third row of the rotation matrix
-    r20 = 2 * b * d - 2 * a * c
-    r21 = 2 * c * d + 2 * a * b
-    r22 = a ** 2 - b ** 2 - c ** 2 + d ** 2
-
-    # 3x3 rotation matrix
-    rot_matrix = np.array([[r00, r01, r02],
-                           [r10, r11, r12],
-                           [r20, r21, r22]])
-    return rot_matrix
+import rosbag
 
 
 def data_for_cylinder_along_z(center_x, center_y, radius_c, height_z):
@@ -40,26 +14,20 @@ def data_for_cylinder_along_z(center_x, center_y, radius_c, height_z):
     return x_grid, y_grid, z_grid
 
 
-data = pd.read_csv(r'C:\Users\frank\Documents\TU Delft\Year 2\Q3\Project\Github\1\optitrack-pose.csv')
-obstacle_data = pd.read_csv(r'C:\Users\frank\Documents\TU Delft\Year 2\Q3\Project\Github\trial_overview.csv')
+def bagselect(bagnum):
+    # input trajectory coordinates, in RHS
+    x_coordinates = []
+    y_coordinates = []
+    z_coordinates = []
+    with rosbag.Bag("Bags/" + bagnum) as bag:
+        for topic, msg, t in bag.read_messages(topics=['/optitrack/pose']):
+            x_coordinates.append(msg.pose.position.x)
+            y_coordinates.append(msg.pose.position.y)
+            z_coordinates.append(msg.pose.position.z)
+    return x_coordinates, y_coordinates, z_coordinates
 
-# input trajectory coordinates, in RHS
-x_coordinates = data['pose.position.x']
-y_coordinates = data['pose.position.y']
-z_coordinates = data['pose.position.z']
 
-timestamps = data['Time']
-
-# input rotations in quaternion system
-x_orientation = data['pose.orientation.x']
-y_orientation = data['pose.orientation.y']
-z_orientation = data['pose.orientation.z']
-w_orientation = data['pose.orientation.w']
-
-for i in range(len(x_coordinates)):
-    Q = [w_orientation[i], x_orientation[i], y_orientation[i], z_orientation[i]]
-    E = quaternion_rotation_matrix(Q)  # rotation matrices from quaternions
-
+obstacle_data = pd.read_csv('C:/Users/ic120/PycharmProjects/AE2223-B11/Bags/trial_overview.csv')
 # input column coordinates, in RHS
 x_column = obstacle_data["Obstacle x"]
 x_column = x_column.drop_duplicates()
@@ -70,41 +38,26 @@ z_column = z_column.drop_duplicates()
 
 # column coordinates in single array, x-y-z for the 3 variants
 column_variants = np.stack((x_column, y_column, z_column), axis=1)
-print(column_variants)
 Xc1, Zc1, Yc1 = column_variants[0]  # first column
 Xc2, Zc2, Yc2 = column_variants[1]  # second column
 Xc3, Zc3, Yc3 = column_variants[2]  # third column
 
 # making 2D plot:
 figure, axes = plt.subplots()
-radius = 0.1
-c = plt.Circle((Xc1, Yc1), radius)
-cc = plt.Circle((Xc2, Yc2), radius)
-ccc = plt.Circle((Xc3, Yc3), radius)
-plt.xlim((-5, 5))
+radius = 0.4
+c = plt.Circle((Xc1, Yc1), radius, color="orange", alpha=0.5)
+cc = plt.Circle((Xc2, Yc2), radius, color="r", alpha=0.5)
+ccc = plt.Circle((Xc3, Yc3), radius, color="b", alpha=0.5)
+
+folder = os.listdir("Bags")
+x, y, z = bagselect("41.bag")
+
+plt.plot(x, z, color= "g")
+plt.xlim((5, -5))
 plt.ylim((-5, 5))
+plt.xlabel("x-coordinate position [m]", fontsize = 15, fontname = "DejaVu Sans")
+plt.ylabel("z-coordinate position [m]", fontsize = 15, fontname = "DejaVu Sans")
 axes.add_artist(c)
 axes.add_artist(cc)
 axes.add_artist(ccc)
 plt.show()
-
-# making 3D plot:
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-height = 2
-
-Xc, Yc, Zc = data_for_cylinder_along_z(Xc1, Yc1, radius, height)  # first column 
-Xcc, Ycc, Zcc = data_for_cylinder_along_z(Xc2, Yc2, radius, height)  # second column 
-Xccc, Yccc, Zccc = data_for_cylinder_along_z(Xc3, Yc3, radius, height)  # third column 
-
-ax.plot_surface(Xc, Yc, Zc)
-ax.plot_surface(Xcc, Ycc, Zcc)
-ax.plot_surface(Xccc, Yccc, Zccc)
-
-fig.suptitle('3D Trajectory', fontsize=20)
-
-ax.set_xlim3d(-5, 5)
-ax.set_ylim3d(-5, 5)
-ax.set_zlim3d(0, 2)
-plt.show()
-
